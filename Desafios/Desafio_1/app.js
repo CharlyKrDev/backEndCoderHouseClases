4e22aa2c60de879ccd4df68a8eee4b365273a45c
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-03-23 23:07:01 +0100

    PrimerDesafioTernario

--- Desafios/Desafio_1/app.js
+++ Desafios/Desafio_1/app.js
@@ -51,5 +51,8 @@ console.log(productManager.getProductId(1));
 productManager.addProduct('Leche', 'La serenisima', 1500, `https://media.f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/i/m/imageedit_1_4837957539.png`)
 productManager.addProduct('La campagnola', 'Mermelada de frutilla', 2500, `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMnutC-MoSpAqdDf3VGm6YmmajggcTwpozAlajyZlnZw&s`, 'LCMF454', 5)
 console.log(productManager.getProducts());
-console.log(`Ternario`);
 console.log(productManager.getProductId(2));
+productManager.addProduct('Taragui', 'Yerba Mate - 500gr', 500, `https://www.google.com/url?sa=i&url=https%3A%2F%2Fyerbamateitalia.it%2Fyerba-mate-%2F16-yerba-mate-taragui-.html&psig=AOvVaw0T8wwks58mD2bm9sK37ZRa&ust=1711317795276000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIipgOKxi4UDFQAAAAAdAAAAABAE`, 'YMT500GC', 5)
+
+console.log(productManager.getProducts());
+
