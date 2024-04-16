e0bc686987aa8546e83000d2e151aaadf734b8d5
CharlyKrDev <charlykrdev@gmail.com>
2024-04-16 21:50:23 +0200

    Desafio 3

--- Desafios/Desafio_3/app.js
+++ Desafios/Desafio_3/app.js
@@ -19,21 +19,23 @@ app.get("/products", (req, res) => {
                 const prodDisplay = mostrar
                     .map(
                         (prod) =>
-                            `<section style="display: flex; flex-direction: column; align-items: center; border: 2px solid black ; background-color: rgba(128, 128, 128, 0.555);">
+                        `<section style="display: flex; flex-direction: column; align-items: center; border: 2px solid black ; background-color: rgba(128, 128, 128, 0.555);">
                         <img style="height: 100px; width: 100px; aspect-ratio: 1;" src=${prod.thumbnail} alt="">
                         <h2 style="color:black; font-weight: bold;">${prod.title}</h2>
+                        <section style="display: flex; flex-direction: column; justify-items: left>
                         <p style="font-weight:500">${prod.description}</p>
-                        <p style="font-weight:200">Stock: ${prod.stock}</p>
-                        <p style="color: red; font-weight: bold; font-size=16px;">$ ${prod.price}</p>
+                        <p style="font-weight:500 text-align: left">Stock: ${prod.stock}</p>
+                        <p style="color: red; font-weight:bold; font-size=16px">$ ${prod.price}</p>
+                        </section>
                         </section>`
                     )
                     .join("");
 
                 const prodGrid = `
-                        <h1>Productos</h1>
-                         <main style="display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); width: 100%; gap:8px;">
+                <h1>Productos</h1>
+                <main style="display: grid; grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); width: 100%; gap:8px;">
                         ${prodDisplay}    
-                        </main>`;
+                </main>`;
 
                 res.setHeader("Content-Type", "text/html");
                 res.send(prodGrid);
@@ -54,11 +56,11 @@ app.get("/products/:pid", (req, res) => {
             .getProductId(id)
             .then((productos) => {
 
-                if (!productos) return res.send(`Producto No encontrado`);
+                if (!productos) return res.send(`El id: ${id} no pertenece a ningún producto`);
                 res.setHeader("Content-Type", "text/html");
 
                 const prodCard =
-                `<main>
+                    `<main>
                 <h2>Resultado:<h2/>
                 <section style=" padding:10px; display: flex; flex-direction: column; max-width:300px; margin:auto; align-items: center; border: 2px solid black ; background-color: rgba(128, 128, 128, 0.555);">
                 <img style="height: 100px; width: 100px; aspect-ratio: 1;" src=${productos.thumbnail} alt="">
@@ -68,7 +70,7 @@ app.get("/products/:pid", (req, res) => {
                     <p style="color: red; font-weight: bold; font-size=16px;">$ ${productos.price}</p>
                     </section>
                 </main>`
-                
+
 
 
                 res.send(prodCard);
@@ -81,6 +83,7 @@ app.get("/products/:pid", (req, res) => {
 
     getProductId();
 });
+
 
 
 app.listen(PORT, () => {
