0ad951b232ec146576b57708e8dcc7d460c94a16
CharlyKrDev <charlykrdev@gmail.com>
2024-05-03 01:55:09 +0200

    Clase 12 - Mongo DB II

---
+++ clase_14/teoria.js
@@ -0,0 +1,16 @@
+/**
+ * CRUD MONGO DB
+ * Glosario:
+ * 
+ * Base de datos:
+ * Modelo relacional:
+ * Modelo no relacional:
+ * Mongo DB:
+ * Mongo --dbpath <name>:
+ * Mongo( en CLI):
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
